11486bbd28de4d72a40c0b30ecdd04bde3122fdf
Giovani Gonzalez <ggonzalez@itexico.net>
2016-03-12 10:38:11 -0600

    Separacion de Autentificacion y validacion, Creacion de Activity List y soporte de Lenguajes

---
+++ app/src/main/java/com/giovani/ejemplos/List.java
@@ -0,0 +1,15 @@
+package com.giovani.ejemplos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DarkGeat on 3/12/2016.
+ */
+public class List extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
--- app/src/main/java/com/giovani/ejemplos/Login.java
+++ app/src/main/java/com/giovani/ejemplos/Login.java
@@ -1,5 +1,6 @@
 package com.giovani.ejemplos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +15,7 @@ public class Login extends AppCompatActivity {
 
     private EditText userName,password;
     private Button login;
+    private String user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +29,34 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String pass = password.getText().toString();
-                String user = userName.getText().toString();
-                if (user.equalsIgnoreCase("")){
-                    userName.setError("Este campo es necesario");
-                    userName.requestFocus();
-                }
-                if (pass.equalsIgnoreCase("")){
-                    password.setError("Este campo es necesario");
-                    password.requestFocus();
-                }
-                if (user.equalsIgnoreCase("giovas") && pass.equalsIgnoreCase("admin")){
-                    Toast.makeText(Login.this,"Login exitoso!!",Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(Login.this,"Fallo de Autentificacion!!",Toast.LENGTH_LONG).show();
+                pass = password.getText().toString();
+                user = userName.getText().toString();
+                if (Validation()) {
+                    if (user.equalsIgnoreCase("giovas") && pass.equalsIgnoreCase("admin")) {
+                        Toast.makeText(Login.this, getString(R.string.login_successfuly), Toast.LENGTH_LONG).show();
+                        Intent listIntent = new Intent(Login.this,List.class);
+                        startActivity(listIntent);
+                    } else {
+                        Toast.makeText(Login.this, getString(R.string.error_bad_credentials), Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
 
+    }
+
+    private boolean Validation(){
+        boolean success = true;
+        if (user.equalsIgnoreCase("")){
+            userName.setError(getString(R.string.error_field_required));
+            userName.requestFocus();
+            success = false;
+        }
+        if (pass.equalsIgnoreCase("")){
+            password.setError(getString(R.string.error_field_required));
+            password.requestFocus();
+            success = false;
+        }
+        return success;
     }
 }
